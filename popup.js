9cbeae72c09e0d79bdd473883a2d72278a3d43b0
shreyashgupta <shreyashgupta88@gmail.com>
2022-04-17 00:52:14 +0530

    working fix

--- popup.js
+++ popup.js
@@ -1,18 +1,55 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-    var stopButton = document.getElementById('stop-callback');
-    stopButton.addEventListener('click', function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, "stop");
-          });
-    }, false);
-    var startButton = document.getElementById('start-callback');
-    startButton.addEventListener('click', function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, "start");
-          });
-    }, false);
+  let started=0;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.storage.sync.get([tabs[0].id.toString()], function(result) {
+      started=parseInt(result[tabs[0].id.toString()])
+      // document.getElementById("h").innerHTML=started
+
+      document.getElementById("h").innerHTML=started
+      var btn = document.getElementById('btn');
+      btn.addEventListener('click', toggle);
+      if(started)
+      {
+        document.getElementById("btn").innerHTML = 'Stop'
+      }
+      else
+      {
+        document.getElementById("btn").innerHTML = 'Start'
+      }
+      });
+  });
   }, false);
 
+function toggle()
+{
+    let started
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.storage.sync.get([tabs[0].id.toString()], function(result) {
+    started=parseInt(result[tabs[0].id.toString()])
+    if(started)
+    {
+        document.getElementById("btn").innerHTML = 'Start'
+        chrome.tabs.sendMessage(tabs[0].id, "stop");
+        let st=tabs[0].id.toString();
+        var jsonfile = {};
+        jsonfile[st] = 0;
+        chrome.storage.sync.set(jsonfile);
+        started=0;
+    }
+    else
+    {
+        document.getElementById("btn").innerHTML = 'Stop'
+        chrome.tabs.sendMessage(tabs[0].id, "start");
+        let st=tabs[0].id.toString();
+        var jsonfile = {};
+        jsonfile[st] = 1;
+        chrome.storage.sync.set(jsonfile);
+        started=1;
+    }
+    });
+});
+}
   
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let started=false;
+let list=[];
 
+let get_xpath=(ele)=>
+{
+  return ele;
+}
+window.addEventListener("keydown", function(e) {
+  //tested in IE/Chrome/Firefox
+  if(e.shiftKey && e.code=='KeyW')
+  {
+    ele=document.getElementsByClassName("hoverr");
+    console.log(e,ele);
+    x_path=get_xpath(ele)
+    list.push(x_path);
+  }
+})
 let start=()=>
 {
   // console.log('here');
   $('body').children().mouseover(function(e){
     $(".hoverr").removeClass("hoverr");     
-    console.log(e)
     $(e.target).addClass("hoverr");
   return false;
   }).mouseout(function(e) {
@@ -17,6 +31,8 @@ let stop=()=>
   $('.hoverr').removeClass("hoverr"); 
   $('body').children().off('mouseover')
   $('body').children().off('mouseout')
+  console.log(list)
+  list=[]
 }
 let recvCallback=(message,sender,response)=>
 {
